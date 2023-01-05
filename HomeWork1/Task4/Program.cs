3c488a9389b356526fe15b0b7f438ab372cfc92f
Denis <ust.hist@gmail.com>
2023-01-05 17:48:08 +0500

    Задача4 решена

--- HomeWork1/Task4/Program.cs
+++ HomeWork1/Task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+WriteLine("Введите число c: ");
+int c = int.Parse(ReadLine()!);
+
+if (a > b & a > c)
+{
+    WriteLine($"{a}");
+}
+if (b > a & b > c)
+{
+    WriteLine($"{b}");
+}
+if (c > a & c > b)
+{
+    WriteLine($"{c}");
+}
