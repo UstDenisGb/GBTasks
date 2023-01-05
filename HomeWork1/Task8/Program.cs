3e2e9e3f6be8e3e1f15871450f61f43b05673c66
Denis <ust.hist@gmail.com>
2023-01-05 18:30:49 +0500

    Задача8 решена

--- HomeWork1/Task8/Program.cs
+++ HomeWork1/Task8/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число N: ");
+int a = int.Parse(ReadLine()!);
+int b = 1;
+
+while (b < a+1)
+{
+    if (b%2 == 0) 
+    {
+        WriteLine($"{b}");
+    }
+    b = b + 1;
+}
