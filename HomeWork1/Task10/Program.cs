9f4927e0ed8e17b9c1dec93871fc932fecddd652
Denis <ust.hist@gmail.com>
2023-01-08 23:43:07 +0500

    Задача 10 решена

--- HomeWork1/Task10/Program.cs
+++ HomeWork1/Task10/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите трехзначное число a: ");
+int a = int.Parse(ReadLine()!);
+
+if (a>99&a<1000) 
+{
+    int a1 = (a%100)/10;
+    WriteLine($"Вторая цифра числа {a} - {a1}");
+}
+else 
+{
+    WriteLine($"Это не трехзначное число");
+}
+
+
