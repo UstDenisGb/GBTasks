f596b2f64f64dadf4ba5d407cf865a897fbdb813
Denis <ust.hist@gmail.com>
2023-01-09 00:20:01 +0500

    Задача 15 решена

--- HomeWork2/Task15/Program.cs
+++ HomeWork2/Task15/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите цифру дня недели: ");
+int a = int.Parse(ReadLine()!);
+
+if (a>0&a<8) 
+{
+    WriteLine(a==6|a==7?"Выходной":"Рабочий");
+}
+else 
+{
+    WriteLine($"Это не день недели");
+}
