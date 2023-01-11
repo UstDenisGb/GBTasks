a5ec81ac4836614a95cfb49a5ede8915f51e8cd0
Denis <ust.hist@gmail.com>
2023-01-12 00:19:54 +0500

    Задача 19 решена

--- HomeWork3/Task19/Program.cs
+++ HomeWork3/Task19/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите пятизначное число: ");
+int a = int.Parse(ReadLine()!);
+int reverse = 0;
+int a2 = a;
+
+while(a2>0) 
+{
+    reverse *= 10;
+    reverse += a2%10;
+    a2 = a2/10;
+}
+if(a==reverse) 
+{
+    WriteLine("Это палиндром");
+}
+else 
+{
+    WriteLine("Это не палиндром");
+}
