fbd0fead99616682be46b48acfa61930c67e5c70
Denis <ust.hist@gmail.com>
2023-01-11 23:26:01 +0500

    Задача 21 решена

--- HomeWork3/Task21/Program.cs
+++ HomeWork3/Task21/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите координату x точки А: ");
+int.TryParse(ReadLine(),out int ax);
+WriteLine("Введите координату y точки А: ");
+int.TryParse(ReadLine(),out int ay);
+WriteLine("Введите координату z точки А: ");
+int.TryParse(ReadLine(),out int az);
+WriteLine("Введите координату x точки B: ");
+int.TryParse(ReadLine(),out int bx);
+WriteLine("Введите координату y точки B: ");
+int.TryParse(ReadLine(),out int by);
+WriteLine("Введите координату z точки B: ");
+int.TryParse(ReadLine(),out int bz);
+
+double length = Math.Sqrt(Math.Pow((bx-ax),2)+Math.Pow((by-ay),2)+Math.Pow((bz-az),2));
+WriteLine(length);
+
