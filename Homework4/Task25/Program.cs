10c7c9b9e8fee9235cc91542b2b8985f6e658589
Denis <ust.hist@gmail.com>
2023-01-17 19:25:09 +0500

    Задача 25 решена

--- Homework4/Task25/Program.cs
+++ Homework4/Task25/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+int degree = GetDegree(a);
+WriteLine($"{degree}");
+
+int GetDegree (int C) 
+{
+    int result = 1;
+    for (int i = 1; i<=b; i++) 
+    {
+        result *= a;
+    }
+    return result;
+}
