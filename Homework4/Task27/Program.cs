cfecf4495b69ce410075eb210a19ab6cb3dfa967
Denis <ust.hist@gmail.com>
2023-01-17 19:45:38 +0500

    Add Task27

--- Homework4/Task27/Program.cs
+++ Homework4/Task27/Program.cs
@@ -1,18 +1,2 @@
-﻿using static System.Console;
-
-WriteLine("Введите число a: ");
-int a = int.Parse(ReadLine()!);
-
-int SumNumber = GetSumNumber(a);
-WriteLine($"{SumNumber}");
-
-int GetSumNumber (int C) 
-{
-    int result = 0;
-    while (a > 0)
-    {
-        result += a%10;
-        a = a/10;
-    }
-    return result;
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
