2b2d58d6c5415a6354a81ece937f3a6670789f3c
Denis <ust.hist@gmail.com>
2023-01-17 20:19:33 +0500

    Задача 29 решена

--- Homework4/Task29/Program.cs
+++ Homework4/Task29/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int [] ar = new int [8];
+FullArray(ar);
+PrintArray(ar);
+
+void FullArray (int [] array) 
+{
+    for (int i = 0; i < 8; i++) 
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+void PrintArray (int [] array) 
+{
+    for (int i = 0; i < 8; i++) 
+    {
+        Write($"{array[i]} ");
+    }
+}
+
