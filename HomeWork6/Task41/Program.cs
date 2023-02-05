493a52af5e53d802f904343280a5a3fc8a00ba8e
Denis <ust.hist@gmail.com>
2023-02-05 17:22:15 +0500

    Задача 41 решена

--- HomeWork6/Task41/Program.cs
+++ HomeWork6/Task41/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите числа через пробел: ");
+
+int [] ar = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+int count = 0;
+
+for (int i = 0; i < ar.Length; i++)
+{
+    if (ar[i] > 0) 
+    {
+        count++;
+    }
+}
+
+WriteLine ($"{count}");
