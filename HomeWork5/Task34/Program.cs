27bf4ae0cbca1f69db6291da748189aaa240e8e5
Denis <ust.hist@gmail.com>
2023-02-05 13:46:21 +0500

    Задача 34 решена

--- HomeWork5/Task34/Program.cs
+++ HomeWork5/Task34/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую методы формирования массива, 
+// заполненного случайными положительными трёхзначными числами, и подсчета количества чётных чисел в массиве.
+
+using static System.Console;
+
+int [] ar = new int [15];
+
+FullArray(ar);
+PrintArray(ar);
+
+WriteLine();
+int EvenNum = 0;
+foreach (int a in ar)
+{
+    EvenNum += a%2==0 ? 1 : 0;
+}
+WriteLine ($"{EvenNum}");
+
+void FullArray (int [] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+}
+void PrintArray (int [] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        Write($"{array[i]} ");
+    }
+}
