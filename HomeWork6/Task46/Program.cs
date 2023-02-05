854f8082212d3ffe25f4024b7378f82cff5ebfb7
Denis <ust.hist@gmail.com>
2023-02-05 17:31:53 +0500

    Задача 46 решена

--- HomeWork6/Task46/Program.cs
+++ HomeWork6/Task46/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+using static System.Console;
+Clear();
+
+WriteLine("Введите b1: ");
+double b1 = double.Parse(ReadLine()!);
+WriteLine("Введите k1: ");
+double k1 = double.Parse(ReadLine()!);
+WriteLine("Введите b2: ");
+double b2 = double.Parse(ReadLine()!);
+WriteLine("Введите k2: ");
+double k2 = double.Parse(ReadLine()!);
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+WriteLine ($"{x}, {y}");
