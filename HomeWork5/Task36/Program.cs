5e085ad13f1305368e3d65112afe7aee96e3419a
Denis <ust.hist@gmail.com>
2023-02-05 13:54:54 +0500

    Add Task36

--- HomeWork5/Task36/Program.cs
+++ HomeWork5/Task36/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, реализующую методы формирования массива, заполненного случайными числами и подсчета суммы элементов, стоящих на нечётных позициях
+using static System.Console;
+
+int [] ar = new int [4];
+
+FullArray(ar);
+PrintArray(ar);
+
+WriteLine();
+int UnevenSum = 0;
+foreach (int a in ar)
+{
+    UnevenSum += a%2==0 ? 1 : 0;
+}
+WriteLine ($"{EvenNum}");
+
+void FullArray (int [] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        array[i] = new Random().Next();
+    }
+}
+void PrintArray (int [] array) 
+{
+    for (int i = 0; i < array.Length; i++) 
+    {
+        Write($"{array[i]} ");
+    }
+}
