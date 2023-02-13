7f9717a07c6d3307824fb9f28629ae90b68be00d
Denis <ust.hist@gmail.com>
2023-02-13 21:53:05 +0500

    Задача 56 решена

--- HomeWork8/Task56/Program.cs
+++ HomeWork8/Task56/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+using static System.Console;
+Clear();
+
+WriteLine("Введите M: ");
+int M = int.Parse(ReadLine()!);
+WriteLine("Введите N: ");
+int N = int.Parse(ReadLine()!);
+
+int [,] ar = new int [M, N];
+FullArray(ar);
+PrintArray(ar);
+
+WriteLine($"{PrintMinSumRow (ar)+1} - строкa с наименьшей суммой элементов");
+
+void FullArray (int [,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+void PrintArray (int [,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]}  ");
+        }
+    WriteLine();
+    }
+}
+int PrintMinSumRow(int [,] array)
+{
+    int row = 0;
+    int minsum = 0;
+    for (int i = 1; i < array.GetLength(1); i++)
+    {
+        minsum += array [0, i];
+    }
+    for (int i = 1; i < array.GetLength(0); i++) 
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i,j];
+        }
+    if (minsum > sum) 
+    {
+        minsum = sum;
+        row = i;
+    }
+    }
+    return row;
+}
