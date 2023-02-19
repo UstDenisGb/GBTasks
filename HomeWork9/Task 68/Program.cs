43e207ad5a9bc54d53b316f5d03ec11c8a93df4f
Denis <ust.hist@gmail.com>
2023-02-19 21:09:35 +0500

    Задача 68 решена

--- HomeWork9/Task 68/Program.cs
+++ HomeWork9/Task 68/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу реализующую метод вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+using static System.Console;
+Clear();
+
+WriteLine("Введите M: ");
+int M = int.Parse(ReadLine()!);
+WriteLine("Введите N: ");
+int N = int.Parse(ReadLine()!);
+
+WriteLine(Akk(M, N));
+
+int Akk (int num1, int num2) 
+{
+    if (num1 == 0) return num2+1;
+    else 
+    {
+        if ((num1 != 0) && (num2 == 0)) return Akk(num1 - 1, 1);
+        else return Akk(num1 - 1, Akk(num1, num2 - 1));
+    }
+}
