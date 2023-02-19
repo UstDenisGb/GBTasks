b15641b93e6621a0d748764701245725eef199a9
Denis <ust.hist@gmail.com>
2023-02-19 20:26:37 +0500

    Задача 64 решена

--- HomeWork9/Task 64/Program.cs
+++ HomeWork9/Task 64/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая реализует рекурсивный метод вывода всех натуральных чисел в промежутке от N до 1
+using static System.Console;
+Clear();
+
+WriteLine("Введите N: ");
+int N = int.Parse(ReadLine()!);
+
+WriteLine(GetNumbers(N));
+
+string GetNumbers (int num) 
+{
+    if (num==1) return num.ToString();
+    else return $"{num},{GetNumbers(num-1)}";
+}
